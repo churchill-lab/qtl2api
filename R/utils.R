469654af3d2fa10d30153c86f7597bf81bcd524f
Matt Vincent <mvincent@jax.org>
2022-04-21 15:18:16 -0400

    id_exists no longer takes a dataset id

--- R/utils.R
+++ R/utils.R
@@ -769,18 +769,17 @@ get_dataset_stats <- function() {
 #' Check if id exists and has data in dataset.
 #'
 #' @param id the id to check
-#' @param ds a dataset object
 #'
 #' @return `TRUE` if id contains data, `FALSE` otherwise
 #' @export
-id_exists <- function(id, ds = NULL) {
+id_exists <- function(id) {
     ret <- list()
 
-    if (valid(ds)) {
-        if (is.character(ds)) {
-            ds <- get_dataset_by_id(ds)
-        }
-        ds <- synchronize_dataset(ds)
+    datasets <- utils::apropos('^dataset\\.*', ignore.case = TRUE)
+
+    for (d in datasets) {
+        ds <- synchronize_dataset(get_dataset_by_id(d))
+        all_ids <- NULL
 
         if (ds$datatype == 'mrna') {
             all_ids <- ds$annot_mrna$gene_id
@@ -798,32 +797,6 @@ id_exists <- function(id, ds = NULL) {
                 dataset_name = ds$display_name,
                 id           = id
             )
-        }
-    } else {
-        # check all datasets
-        datasets <- utils::apropos('^dataset\\.*', ignore.case = TRUE)
-
-        for (d in datasets) {
-            ds <- synchronize_dataset(get_dataset_by_id(d))
-            all_ids <- NULL
-
-            if (ds$datatype == 'mrna') {
-                all_ids <- ds$annot_mrna$gene_id
-            } else if(ds$datatype == 'protein') {
-                all_ids <- ds$annot_protein$gene_id
-            } else if(ds$datatype == 'phos') {
-                all_ids <- ds$annot_phos$gene_id
-            } else if(ds$datatype == 'phenotype') {
-                all_ids <- ds$annot_phenotype$data_name
-            }
-
-            if (any(id == colnames(ds$data)) && (id %in% all_ids)) {
-                ret[[d]] <- list(
-                    dataset_id   = d,
-                    dataset_name = ds$display_name,
-                    id           = id
-                )
-            }
         }
     }
 
